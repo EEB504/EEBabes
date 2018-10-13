f414d416ec67c4937b551b2b652eaebec1400baf
clara-howell <chowel30@vols.utk.edu>
2018-10-13 16:01:01 -0400

    Update dirtplot.R

---
+++ dirtplot.R
@@ -0,0 +1,12 @@
+dirtdata<-read.csv("dirtivore.csv")
+plot(dirtdata$MassKG,dirtdata$rMax)
+masslog<-log10(dirtdata$MassKG)
+rlog<-log10(dirtdata$rMax)
+model1<-lm(rlog~masslog)
+plot1<-ggplot()+
+  geom_point(aes(masslog,rlog), colour="grey")+
+  xlab(expression('Body Mass (Log'[10]*'KG)'))+
+  ylab(expression('r'[max]*' (Log'[10]*'transformed)'))+
+  geom_abline(intercept=-0.0859,slope = -0.20229)+
+  
+  
